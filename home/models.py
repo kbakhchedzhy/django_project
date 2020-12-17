73096c264228050599018388970a21fc124d6e77
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2020-12-17 16:47:44 +0300

    new command

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    description = models.TextField()
+    birthday = models.DateField()
+    email = models.CharField(max_length=200)
