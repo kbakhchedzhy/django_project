0d4654b1d8b2ba4fac3f9703cdddcb4922e3d3b4
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2020-12-21 16:18:46 +0200

    fixed

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Student(models.Model):
 
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True) # noqa
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     age = models.IntegerField()
