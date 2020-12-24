1db5ef40a9624089beb3c45298d05568c19177a2
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2020-12-25 00:34:07 +0200

    finish

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin # noqa
-# noqa
-# Register your models here.
+from django.contrib.admin import ModelAdmin
+from django.utils.html import format_html
+
+from home.models import Student
+
+
+class StudentAdmin(ModelAdmin):
+    list_display = ('full_name', 'email', 'birthday')
+    ordering = ('name',)
+
+    def full_name(self, instance):
+        if instance.social_url:
+            # Correy Gomez has url
+            return format_html("<a href = \"{}\">{} {}</a>", instance.social_url, instance.name, instance.surname)
+        else:
+            return "{} {}".format(instance.name, instance.surname)
+
+
+admin.site.register(Student, StudentAdmin)
